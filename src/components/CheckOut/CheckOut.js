c4356c93fa9c00b5bd319d9d5803393679588cc4
solo-mizan <mizanbd.eee@gmail.com>
2022-04-17 01:24:25 +0600

    components are set

---
+++ src/components/CheckOut/CheckOut.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CheckOut.css';
+
+const CheckOut = () => {
+    return (
+        <div>
+            <h2>Check out here</h2>
+        </div>
+    );
+};
+
+export default CheckOut;
