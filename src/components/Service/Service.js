1645d801fe3dee108e559e33d4554e7e3f74e7d3
solo-mizan <mizanbd.eee@gmail.com>
2022-04-17 06:31:37 +0600

    services page data loaded

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 import './Service.css';
 
-const Service = () => {
+const Service = ({ service }) => {
+    
+    const { name, price, picture, description } = service;
+
     return (
         <div>
-            <h2>Individual service here</h2>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={picture} />
+                <Card.Body>
+                    <Card.Title className='text-primary'>{name}</Card.Title>
+                    <Card.Text>
+                        {description}
+                    </Card.Text>
+                    <h3>Price: {price} taka</h3>
+                    <Button variant="success">Book Now</Button>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 import './Services.css';
 
 const Services = () => {
-const [services, setServices] = useState([]);
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
 
     return (
-        <div>
-            <h2>Services list here</h2>
+        <div className='service-cards'>
+            {
+                services.map(service => <Service
+                    key={service._id}
+                    service={service}
+                ></Service>)
+            }
         </div>
     );
 };
