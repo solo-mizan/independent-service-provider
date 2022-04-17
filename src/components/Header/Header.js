b955fced2261fbd26f460f6c6e5418ca7b8a4546
solo-mizan <mizanbd.eee@gmail.com>
2022-04-17 22:17:26 +0600

    about me section added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import Services from '../Services/Services';
 import './Header.css';
 
 const Header = () => {
     return (
-        <Navbar bg="light" expand="lg" sticky='top'>
+        <Navbar collapseOnSelect expand="lg" bg="light" sticky='top'>
             <Container>
                 <Navbar.Brand className='text-primary' as={Link} to={'/home'}>Agricultural Consult Services</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to={'/home'}>Home</Nav.Link>
-                        <Nav.Link as={Link} to={'/services'}>Services</Nav.Link>
-                        <Nav.Link as={Link} to={'/reviews'}>Reviews</Nav.Link>
-                        <Nav.Link as={Link} to={'/blogs'}>Blogs</Nav.Link>
-                        <Nav.Link as={Link} to={'/about'}>About me</Nav.Link>
-                        <Nav.Link as={Link} to={'/login'}>Login</Nav.Link>
+                        <Nav.Link eventKey="1" as={Link} to={'/home'}>Home</Nav.Link>
+                        <Nav.Link eventKey="2" as={Link} to={'/services'}>Services</Nav.Link>
+                        <Nav.Link eventKey="3" as={Link} to={'/reviews'}>Reviews</Nav.Link>
+                        <Nav.Link eventKey="4" as={Link} to={'/blogs'}>Blogs</Nav.Link>
+                        <Nav.Link eventKey="5" as={Link} to={'/about'}>About me</Nav.Link>
+                        <Nav.Link eventKey="6" as={Link} to={'/login'}>Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
