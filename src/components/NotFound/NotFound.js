33135f6c6ccd1a2591d826d5653696909ffb128e
solo-mizan <mizanbd.eee@gmail.com>
2022-04-17 07:20:03 +0600

    404 NotFound route set

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './NotFound.css';
+import errorPic from '../../image/404/404.gif';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404 Not found here</h2>
+        <div className='not-found-div'>
+            <img src={errorPic} alt="404 Not found!" />
+            <h1>404 Not found!</h1>
+            <p><small>The page you're looking for is not found. Check your Internet connection</small></p>
         </div>
     );
 };
